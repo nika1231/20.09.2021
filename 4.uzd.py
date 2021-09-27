2a6ce53729414bea4d77bb9c40f289b965a83eba
nika1231 <70640951+nika1231@users.noreply.github.com>
2021-09-27 13:54:08 +0000

    4.uzd.py

---
+++ 4.uzd.py
@@ -0,0 +1,13 @@
+#uzdevumā nepieciešams definēt divus for ciklus, izmantot if apgalvojumu, lietotāja ievadi un izvadi.
+
+saraksts = sk
+
+saraksts = [int(sk) for sk in input('Ievadi skaitļus, atdalot tos ar atstarpi:')]
+
+saraksts = [int(sk) for sk in input('Ievadi skaitļus, atdalot tos ar atstarpi:'). split()]
+
+for i in range(len(saraksts)):
+  print(i)
+  
+if saraksts[i] == saraksts[i+1]
+  print(saraksts[i],saraksts[i+1])
