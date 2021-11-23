b03d30ecdc26704d7b34078cb42d7a13ab923973
nika1231 <70640951+nika1231@users.noreply.github.com>
2021-11-23 13:12:23 +0000

    pabeidzu programmu par lādīti

--- uzdevums.ladites.py
+++ uzdevums.ladites.py
@@ -1,4 +1,10 @@
+print('Sveiks/a')
+print('šī programma izdrukās vienkāršun rēķinu koka lādītei ar gravējumu\n\n')
+
+
+
 class Rekins:
+
 
    def __init__(self,klients,veltijums,izmers,materials):
    
@@ -9,37 +15,56 @@ class Rekins:
   
 
       self.teksta_gar = len(self.veltijums)
-      self.ladites_izm = self.izmeri.split(',')
-      self.garums = self.ladites_izm[0]
-      self.platums = self.ladites_izm[1]
-      self.augstums = self.ladites_izm[2]
+      self.ladites_izm = self.izmers.split(',')
+      self.garums = int(self.ladites_izm[0])
+      self.platums = int(self.ladites_izm[1])
+      self.augstums = int(self.ladites_izm[2])
+      self.materials = int(self.materials)
 
 
    def izdrukat(self):
     print(f'Klienta vārds {self.klients}')
-    print(f'veltjuma teksts{self.veltijums}')
+    print(f'veltījuma teksts{self.veltijums}')
     print(f'izmērs{self.izmers}')
-    print(f'lodītes materials{self.materials}')
+    print(f'lodītes materiāls{self.materials}')
+    print("\n\n")
+    print('\033[1m'+"Pasūtītāja dati:"+'\033[0m')
+    print("-"*50)
+    print(f"\x1B[3mPasūtītāja vārds un uzvārds:\x1B[0m \033[1;32m{self.klients}\033[1;0m")
+    print(f"\x1B[3mVeltījuma teksts:\x1B[0m\033[1;32m{self.veltijums}\033[1;0m")
+    print(f"\x1B[3mLādītes izmēri:\x1B[0m\033[1;32m{self.izmers}\033[1;0m")
+    print(f"\x1B[3mPlatums:\x1B[0m\033[1;32m{self.platums}\033[1;0m")
+    print(f"\x1B[3mGarums:\x1B[0m\033[1;32m{self.garums}\033[1;0m")
+    print(f"\x1B[3mAugstums:\x1B[0m\033[1;32m{self.augstums}\033[1;0m")
+    print(f"\x1B[3mMateriāla cena EUR/m2:\x1B[0m\033[1;32m{self.materials}\033[1;0m")
+    print(f"\x1B[3mIzmaksas:\x1B[0m\033[1;32m{self.aprekins()}\033[1;0m")
+
+
+
 
    def aprekins(self):
      darba_samaksa = 15
      PVN = 21
-       produkta_cena = (self.teksta_gar)* 1.2 + (self.platums/100 * self.garums/100 * self.augstums/100)/ 3 * self.materialas
-       PVN_summa = (produkta_cena + darba_samaksa)*PVN/100
-       rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
-     pass
-klients = input('Uzraksti savu vārdu un uzvārdu')
-veltijums = input('Uzraksti veltījumu:')
-izmeri = input('Ievadi lādītes izmēru\n Garums, platums,augstums(raksti veselus skaitļus, atdalot tos ar komatiem\n')
-materials = input('materiala cena EUR/m2:')
+     produkta_cena = (self.teksta_gar)* 1.2 + (self.platums/100 * self.garums/100 * self.augstums/100)/ 3 * self.materials
+     PVN_summa = (produkta_cena + darba_samaksa)*PVN/100
+     rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
+     return rekina_summa
+klients = input('Pasūtītāja vārds,uzvārds:')
+print('-'*50)
+veltijums = input('Nepieciešamais veltījums:')
+print('-'*50)
+izmeri = input('Lādītes izmērs - Garums, platums,augstums- milimetros(raksti veselus skaitļus:')
+print('-'*50)
+materials = input('Kokmateriāla cena EUR/m2:')
 
 rekins = Rekins(klients,veltijums,izmeri, materials)
-print(rekins.klients)
-print(rekins.veltijums)
-print(rekins.izmeri)
-print(rekins.ladites_izm)
+print(rekins.izdrukat())
+# print(rekins.klients)
+# print(rekins.veltijums)
+# print(rekins.izmers)
+# print(rekins.ladites_izm)
 
-print(rekins.aprekins())
+# print(rekins.aprekins())
 # print(izmeri)
 # print(type(izmeri))  
 # print(izmeri.split(','))  
